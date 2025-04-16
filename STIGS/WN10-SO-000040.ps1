20e82447a836c5fe7d843417c63c42718137df88
MontellRicks <monty5731@protonmail.com>
2025-04-16 13:33:16 -0700

    Update WN10-SO-000040.ps1

--- STIGS/WN10-SO-000040.ps1
+++ STIGS/WN10-SO-000040.ps1
@@ -11,7 +11,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : WN10-CC-000295
+    STIG-ID         : WN10-SO-000040
 
 .TESTED ON
     Date(s) Tested  : 
